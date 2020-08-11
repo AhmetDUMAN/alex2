494f3329122bc6a95910029372f1e09423b52f2e
Ahmet <ahmetpower2651@gmail.com>
2020-08-11 12:45:05 +0300

    ikinci projemin çakışması

--- src/alex2/Alex3.java
+++ src/alex2/Alex3.java
@@ -7,7 +7,7 @@ public class Alex3 {
 		System.out.println("He is number the '10'.");
 		System.out.println("remote deÄŸiÅŸiklik");
 		System.out.println("Hocam sizi bekliyorum evet.");
-		
+		System.out.println("Çakışma - hoca");
 	}
 
 }
