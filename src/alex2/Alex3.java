d3b470a50d46e639d226cab17c5eb77c57ac9b07
Ahmet DUMAN <69395432+AhmetDUMAN@users.noreply.github.com>
2020-08-11 12:39:21 +0300

    Update Alex3.java
    alex2 githup'da değişiklik yaptım

--- src/alex2/Alex3.java
+++ src/alex2/Alex3.java
@@ -5,6 +5,7 @@ public class Alex3 {
 	public static void main(String[] args) {
 		System.out.println("Alex unutulmaz bir efsanedir.");
 		System.out.println("He is number the '10'.");
+		System.out.println("remote değişiklik");
 	}
 
 }
